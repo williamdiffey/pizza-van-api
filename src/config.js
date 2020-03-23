47fe2ec1a264e849765b1fc31b9d396df6c52b33
William Diffey <50226488+williamdiffey@users.noreply.github.com>
2020-03-23 20:37:35 +0900

    changed config to try to fix deploy error

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/pizza',
+  DB_URL: process.env.DB_URL,
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
